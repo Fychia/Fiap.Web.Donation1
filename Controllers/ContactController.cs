d5e34471cb95a61caf80879c1ab2548e1607f142
Rafael Valério <rafael.valerio@outlook.com.brgit status~git config --global user.email rafael.valerio@outlook.com.br>
2023-09-12 22:00:50 -0300

    Contact

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Donation1.Controllers;
+
+public class ContactController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Record(ModelContact modelContact)
+    {
+        return View("Sucess");
+    }
+
+    [HttpGet]
+    public IActionResult Help()
+    {
+        return View();
+    }
+}
---
+++ Controllers/ModelContact.cs
@@ -0,0 +1,8 @@
+﻿namespace Fiap.Web.Donation1.Controllers;
+
+public class ModelContact
+{
+    public string Name{ get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+}
